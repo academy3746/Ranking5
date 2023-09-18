c66bbf85b51164a1395203c3c15f2083ea8435b2
ijonghyeon <academy3746@gmail.com>
2023-09-18 10:53:02 +0900

    46. 230918

--- lib/features/webview_controller.dart
+++ lib/features/webview_controller.dart
@@ -300,6 +300,7 @@ class _WebviewControllerState extends State<WebviewController> {
                   }
                   return NavigationDecision.navigate;
                 },
+                //userAgent: "Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1",
               ),
             ),
           );
